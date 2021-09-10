6e33343e1fb9bb6c13335969eb407ef076e45aa1
aragonenred <aragonenred@gmail.com>
2021-09-10 19:15:20 -0300

    Lenovo 1009

--- js/main.js
+++ js/main.js
@@ -2,23 +2,84 @@ document.addEventListener('DOMContentLoaded', function(){
 
     const nav = document.querySelector('#nav-header');
     const hero = document.querySelector('#hero');
-    const poso = nav.offsetTop;
+    const calendar = document.querySelector('#calendar-head');
+    const calendar_title = document.querySelectorAll('.calendar-title');
+    const nav_position = nav.offsetTop;
+    const rwFullText = document.querySelectorAll('.rw-fullText');
 
 
-    window.addEventListener('scroll', function(){
-        altura = nav.offsetHeight;
-        console.log('altura: ' + altura + " scrol: " + window.scrollY + 'Posision:' + nav.offsetTop);
-        if(window.scrollY > poso ){
-            altura = nav.offsetHeight;
-            //console.log('altura: ' + altura + " scrol: " + window.scrollY);
-            hero.style.setProperty('margin-top',altura  + 'px');
-            nav.classList.add('fixed');
-        }else{
-            nav.classList.remove('fixed');
-            hero.style.setProperty('margin-top', '0');
+    for(i=0; i <rwFullText.length; i++){
+        sideUp(i);
+    }
+    function sideUp(index){
+        rwFullText[index].addEventListener('mouseover', function(){
+            rwFullText[index].children[0].style.marginTop ='0';
+        });
+         
+        rwFullText[index].addEventListener('mouseleave', function(){
+            rwFullText[index].children[0].style.marginTop ='50rem';
+        });
+    } 
+    
+    fixNav();
+    setCalendarTitle();
+    showCalendar();
+
+
+
+
+
+    function fixNav(){
+        window.addEventListener('scroll', function(){
+            if(window.scrollY > nav_position ){
+                hero.style.setProperty('margin-top',nav.offsetHeight  + 'px');
+                nav.classList.add('fixed');
+            }else{
+                nav.classList.remove('fixed');
+                hero.style.setProperty('margin-top', '0');
+            }
+        });
+    }
+
+
+
+    function showCalendar(){
+        var date = new Date();
+        for(i=0; i<calendar_title.length; i++){
+            calendar_title[i].addEventListener('click', function(e){
+    
+                if(calendar.getAttribute('hidden') != null){
+                    for(i= 0; i<7; i++){
+                        console.log('tiene hidden');
+                        document.querySelector('#day-' + i).setAttribute('hidden' ,'');
+                    }
+                    calendar.removeAttribute('hidden');
+                }else{
+                    for(i= 0; i<7; i++){
+                        console.log('no tiene hidden');
+                        document.querySelector('#day-' + i).removeAttribute('hidden');
+                        if(date.getDay() == i){
+                            document.querySelector('#day-' + i).classList.add('selectDay');
+                        }
+                    }
+                    calendar.setAttribute('hidden','');
+                }
+        
+            });
+           
+        }
+    }
+
+    function setCalendarTitle(){
+        var date = new Date();
+        
+        for(i= 0; i<7; i++){
+            if(date.getDay() == i){
+                calendar.firstElementChild.innerHTML = document.querySelector('#day-' + i).firstElementChild.innerHTML + ' Today ' + document.querySelector('#day-' + i).children[2].innerText;
+            }
         }
 
-    })
+    }
 
 
 
